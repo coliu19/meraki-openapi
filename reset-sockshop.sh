3614b4a186caa95f0bd05884833fb840df17cac4
Neelesh Pateriya <npateriy@cisco.com>
2022-05-27 14:41:34 -0700

    Cleanup DevRel wear description

--- reset-sockshop.sh
+++ reset-sockshop.sh
@@ -15,12 +15,14 @@ product_tag="DevRel Wear"
 
 echo "## Prepare services"
 for service in ${services[*]}; do
+  # upper case first char
+  service_title="$(tr '[:lower:]' '[:upper:]' <<< ${service:0:1})${service:1}"
 
   echo delete "$service"
   apiregistryctl service delete "$service" --debug || true
   echo create "$service"
 
-  printf -v payload '{ "organization_id": "DevNet", "product_tag": "%s", "name_id": "%s", "title": "%s demo", "description": "%s API for demo a microservice communication in sockshop", "contact": {"name": "Engineering Team", "email": "engineering@merchandiseshop.com", "url": "https://testing-developer.cisco.com/api-registry/reports?service=%s"}, "analyzers_configs": {"drift": {"service_name_id": "%s.sock-shop"}} }' "$product_tag" "$service" "$service" "$service" "$service" "$service"
+  printf -v payload '{ "organization_id": "DevNet", "product_tag": "%s", "name_id": "%s", "title": "%s Demo API", "description": "%s microservice for %s demo application", "contact": {"name": "Engineering Team", "email": "engineering@merchandiseshop.com", "url": "https://app-8081-apiregistry1.devenv-int.ap-ne-1.devnetcloud.com/"}, "analyzers_configs": {"drift": {"service_name_id": "%s.sock-shop"}} }' "$product_tag" "$service" "$service_title" "$service_title" "$product_tag"
   apiregistryctl -H "$host" service create --data "$payload" || true
 done
 apiregistryctl -H "$host" service list
@@ -29,11 +31,13 @@ echo "## Upload the v0.0-rev1 specs which are the base raw specs"
 for service in ${services[*]}; do
   echo updateload spec for "$service"
   apiregistryctl -H "$host" service uploadspec v0.0-rev1/"$service".json -s "$service" --version v0.0 --revision 1
+  sleep 30
 done
 
 echo "## Upload the v0.0-rev2 specs which are the perfect specs"
 for service in ${services[*]}; do
   echo updateload spec for "$service"
   apiregistryctl -H "$host" service uploadspec v0.0-rev2/"$service".json -s "$service" --version v0.0 --revision 2
+  sleep 30
 done
 echo "## Finished prepare works"
